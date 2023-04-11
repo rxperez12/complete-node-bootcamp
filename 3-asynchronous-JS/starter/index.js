a4b9444f91b21f8ea8486f50b0484c08652ac70b
rxperez12 <rxperez12@gmail.com>
2023-04-11 13:13:16 -0700

    got gitignore to work

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -4,5 +4,11 @@ const superagent = require("superagent");
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
-  superagent.get(``);
+  superagent.get(`https://dog.ceo/api/breeds/image/random`).end((err, res) => {
+    console.log(res.body.message);
+
+    fs.writeFile("dog-img.txt", res.body.message, (err) => {
+      console.log("Random dog image saved to file!");
+    });
+  });
 });
