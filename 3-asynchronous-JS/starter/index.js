190a7d5bcea7cc93177018fb055cd5bf893c9da3
rxperez12 <rxperez12@gmail.com>
2023-04-12 14:46:28 -0700

    finished async section

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,14 +1,59 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent.get(`https://dog.ceo/api/breeds/image/random`).end((err, res) => {
-    console.log(res.body.message);
-
-    fs.writeFile("dog-img.txt", res.body.message, (err) => {
-      console.log("Random dog image saved to file!");
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
     });
   });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not write file");
+      resolve("success");
+    });
+  });
+};
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(`https://dog.ceo/api/breeds/image/random`);
+    console.log(res.body.message);
+
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("Random do image save to file!");
+  } catch (err) {
+    console.log(err);
+  }
+  return "2: READY";
+};
+console.log("1: Will get dog pics");
+getDogPic().then((x) => {
+  console.log(x);
+  console.log("3: Done getting dog pics");
 });
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(`https://dog.ceo/api/breeds/image/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then(() => {
+//     console.log("Random dog image saved to file!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
